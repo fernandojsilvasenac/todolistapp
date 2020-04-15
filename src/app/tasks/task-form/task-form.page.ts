613dcba5627ca3b7d281b324db7bef1d7052713d
fernandojsilvasenac <fernando.jsilvasenac@gmail.com>
2020-04-15 17:59:06 -0300

    added or created form html and methods criarFormulario and onSubmit

--- src/app/tasks/task-form/task-form.page.ts
+++ src/app/tasks/task-form/task-form.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TasksService } from './../shared/tasks.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-task-form',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-form.page.scss'],
 })
 export class TaskFormPage implements OnInit {
+  
+  formtasks: FormGroup;
 
-  constructor() { }
+  constructor( private formBuilder: FormBuilder,
+    private toastController: ToastController,
+    private tasksService: TasksService,
+    private router: Router ) { }
 
   ngOnInit() {
+    this.criarFormulario();
   }
 
+  criarFormulario() {
+    this.formtasks = this.formBuilder.group({
+      description: ['', Validators.required],
+      completed: [false],
+    });
+  }
+
+  async show(mensagem: string){
+    const toast = await this.toastController.create({
+      message: mensagem,
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
+
+  async onSubmit() {
+    if (this.formtasks.valid) {
+      console.log(this.formtasks.value);
+      try {
+        await this.tasksService.insert(this.formtasks.value);
+        this.show('Tarefa salva com sucesso !!!');
+        this.criarFormulario();
+        this.router.navigate(['/tasks/list']);
+
+      } catch (error) {
+        console.log(error);
+        this.show('Erro ao tentar Salvar');
+        // this.toastr.error('Erro ao tentar Salvar')
+      }
+
+    }
+
+  }
 }
